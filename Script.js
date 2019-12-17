a8700aa09edaac126847e07fe677b7f006572982
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-17 12:07:56 -0700

    started the psudo codeing in java and structuring the html.

---
+++ Script.js
@@ -0,0 +1,49 @@
+//Start Button: Starts the function to show the first question
+var startButton = $('<button>')
+startButton.text('Start Here!')
+
+
+
+
+
+// Question timer and a global timer 
+
+
+
+
+
+// generate question  generate 4 buttons 
+
+
+
+
+
+
+
+// Weight the answers 
+
+
+
+
+
+
+// make time left add to score if correct
+
+
+
+
+
+// make question-time time subtract from total time
+
+
+
+
+
+
+//add score to local storage 
+
+
+
+
+
+
---
+++ questions.js
@@ -0,0 +1,31 @@
+var questions = [{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"}
+    {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    }{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    }{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    }{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    }{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+    }{
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+}
+
+]
