5b3203122c9ac6baaa028bccdcde73eb9a6d1048
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-17 17:48:23 -0700

    did some clean up on the code, wrote a global timer

--- Script.js
+++ Script.js
@@ -1,12 +1,53 @@
 //Start Button: Starts the function to show the first question
-var startButton = $('<button>')
-startButton.text('Start Here!')
+var startBtn = document.getElementById('header')
+startBtn.addEventListener("click",function(){
+    startBtn.style.display = 'none';
+
+    //run  the quiz function here 
+    setTime()
+})
+
+
 
 
 
 
 
 // Question timer and a global timer 
+var totalTime = questions.length * 15
+console.log( totalTime)
+var timeEl = document.querySelector(".time");
+var mainEl = document.getElementById("main");
+var score = []
+var questionTime = 15;
+
+
+
+
+function setTime() {
+  var timerInterval = setInterval(function() {
+    totalTime--;
+    
+    questionTime--;
+    timeEl.textContent =  " Total time left: " + totalTime;
+    console.log(questionTime)
+    
+    
+    console.log(score)
+    if(totalTime === 0) {
+      clearInterval(timerInterval);
+      sendMessage();
+    }
+
+  }, 1000);
+}
+// how to print to the score, and how to deduct the time if wrong. 
+
+function sendMessage() {
+  
+
+}
+
 
 
 
@@ -15,7 +56,7 @@ startButton.text('Start Here!')
 // generate question  generate 4 buttons 
 
 
-
+var questionDisplay = document.getElementById('question')
 
 
 
--- questions.js
+++ questions.js
@@ -2,27 +2,27 @@ var questions = [{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"}
-    {
+    ,{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
-    }{
+    },{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
-    }{
+    },{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
-    }{
+    },{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
-    }{
+    },{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
-    }{
+    },{
     title: "Commonly used data types DO NOT include:",
     choices: ["strings", "booleans", "alerts", "numbers"],
     answer: "alerts"
