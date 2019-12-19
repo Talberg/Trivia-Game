505de2144cf9af333514c7e8daf793d4d1932cf4
Alexander Talberg <alexandermtalberg@gmail.com>
2019-12-19 09:02:47 -0700

    moved the questions to the top

--- Script.js
+++ Script.js
@@ -2,6 +2,29 @@
 var header = document.getElementById('header')
 var startBtn = document.getElementById('startBtn')
 var highScore = document.getElementById('high-score')
+var questions = [{
+    title: "Question 1: When Walt Disney's seven dwarfs went off to work in the mines, what were they looking for?",
+    choices: ["Gold", "Peals", "Diamonds", "Silver"],
+    answer: "Silver"}
+    ,{
+    title: "Question 2: What is the most common blood type in humans?",
+    choices: ["O", "AB", "A", "B"],
+    answer: "O"
+    },{
+    title: "Question 3: Which company use the android application in their mobiles first time?",
+    choices: ["Motorola", "Sony", "Apple", "HTC"],
+    answer: "HTC"
+    },{
+    title: "Question 4: Where is the largest data center?",
+    choices: ["Nevada, USA", "Langfang,China", "Illinois,USA", "Bangalore,India"],
+    answer: "Nevada,USA"
+    },{
+    title: "Question 5: What is the combined wealth of the richest 5 people in the world?  ",
+    choices: ["140.7bn", "471.8bn", "234.4bn", "803.6bn"],
+    answer: "471.8bn"
+    }
+   
+]
 startBtn.addEventListener("click",function(){
     startBtn.style.display = 'none';
     header.style.display= 'none'
